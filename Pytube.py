9c0bdc393adb098b9394368846f5e3c127b1db8c
Eduardo Gonzales <63215090+EduGonz123@users.noreply.github.com>
2021-10-23 10:05:28 -0600

    Update Pytube.py

--- Pytube.py
+++ Pytube.py
@@ -4,19 +4,21 @@ import os
 while True:
 	os.system("clear")
 	
-	print("\tDescargar videos de YouTube")
-	print("By: Charly GM")
-	url = input("\nInserte el link del video: ")
+	print("\t Descargar videos de YouTube")
+	print(" By: Charly GM")
+	url = input("\n Pegue el link del video que desea descargar: ")
+	
+	caroetaAndroid = "mkdir /sdcard/Descargas CharlyTube"
 	
 	youtube = pytube.YouTube(url)
 	
 	video = youtube.streams.first()
 	
-	video.download()
+	video.download(carpetaAndroid)
 	
-	print("\nEl video ", youtube.title, ", ha sido descargado exitosamente")
+	print("\n El video ", youtube.title, ", ha sido descargado exitosamente")
 	
-	pregunta = input("\n¿Desea descargar otro video? (sí = s o no = n): ")
+	pregunta = int(input("\n ¿Desea descargar otro video? (sí = 1 o no = 2): "))
 	
-	if pregunta == "n" or pregunta == "N":
+	if pregunta == 2:
 		break;
